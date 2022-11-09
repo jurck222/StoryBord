5d92d788886e7c202857428126d29e6ac22be64a
jurck222 <56190152+jurck222@users.noreply.github.com>
2022-11-09 21:39:15 +0100

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using vaja02IS.web.Models;
+using vaja02IS.web.Services;
+
+namespace web.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public JsonFileProductService ProductService;
+    public IEnumerable<Product>Products{get;set;}
+
+    public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+    {
+        _logger = logger;
+        ProductService = productService;
+    }
+
+    public void OnGet()
+    {
+        Products = ProductService.GetProducts();
+    }
+}
